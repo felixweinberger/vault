4f3135c9286bbbb9d36dedba9b1d852fcafac63f
Felix Weinberger <felix.weinberger@gmail.com>
2019-02-27 21:28:44 +0100

    feat(redux): refactored to wrap AppComponent in App to make redux available

--- client/App.js
+++ client/App.js
@@ -1,65 +1,18 @@
 import React from 'react';
-import {
-  Platform,
-  StatusBar,
-  StyleSheet,
-  View,
-} from 'react-native';
-import {
-  AppLoading,
-  Asset,
-  Font,
-  Icon,
-} from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+import AppContainer from './AppContainer';
+import reducers from './redux/reducers';
+
+const store = createStore(reducers, {});
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
   render() {
-    if (!this.state.isLoadingComplete) {
-      return (
-        <AppLoading
-          startAsync={this.loadResourcesAsync}
-          onError={this.handleLoadingError}
-          onFinish={this.handleFinishLoading}
-        />
-      );
-    }
-
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
     );
   }
-
-  loadResourcesAsync = async () => Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Icon.Ionicons.font,
-    }),
-  ]);
-
-  handleLoadingError = (error) => {
-    console.warn(error); // eslint-disable-line no-console
-  };
-
-  handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
---
+++ client/AppContainer.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {
+  AppLoading,
+  Asset,
+  Font,
+  Icon,
+} from 'expo';
+import AppNavigator from './navigation/AppNavigator';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this.loadResourcesAsync}
+          onError={this.handleLoadingError}
+          onFinish={this.handleFinishLoading}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    );
+  }
+
+  loadResourcesAsync = async () => Promise.all([
+    Asset.loadAsync([
+      require('./assets/images/robot-dev.png'),
+      require('./assets/images/robot-prod.png'),
+    ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Icon.Ionicons.font,
+    }),
+  ]);
+
+  handleLoadingError = (error) => {
+    console.warn(error); // eslint-disable-line no-console
+  };
+
+  handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
+
+// const mapStateToProps = (state) => ({
+//   events: state.events,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   updateEvents: events => dispatch(updateEvents(events)),
+//   addEvent: event => dispatch(addEvent(event)),
+// });
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(App);
