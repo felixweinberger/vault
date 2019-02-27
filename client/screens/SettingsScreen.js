9bd37e83e6dd5fce499b59b3e4e828d454e1cc3a
Felix Weinberger <felix.weinberger@gmail.com>
2019-02-27 16:25:18 +0100

    feat(settings): change settings to be a hello world test

--- client/screens/SettingsScreen.js
+++ client/screens/SettingsScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-export default function SettingsScreen() {
-  return <Text>Hello World</Text>;
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Hello',
+  };
+
+  render() {
+    return (
+      <Text>Hello World</Text>
+    );
+  }
 }
