d22dbd5e08e3e41e58dd106529709a43c2d2a617
Felix Weinberger <felix.weinberger@gmail.com>
2019-02-27 16:18:55 +0100

    fix(linting): fix linting errors in screens and remove unneeded stuff from the template

--- client/screens/LinksScreen.js
+++ client/screens/LinksScreen.js
@@ -7,9 +7,17 @@ export default class LinksScreen extends React.Component {
     title: 'Links',
   };
 
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 15,
+      backgroundColor: '#fff',
+    },
+  });
+
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <ScrollView style={this.styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
         <ExpoLinksView />
@@ -17,11 +25,3 @@ export default class LinksScreen extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
