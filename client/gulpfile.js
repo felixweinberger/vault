4928d2477e67fc331edd75052bcec65dfbaff8ef
Felix Weinberger <felix.weinberger@gmail.com>
2019-02-28 15:02:36 +0100

    fix(gulp): remove unneeded legacy lines from gulpfile

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,6 +1,4 @@
 const gulp = require('gulp');
-const fs = require('fs');
-const del = require('del');
 const $ = require('gulp-load-plugins')();
 
 // Utility to ignore unnecessary files
@@ -20,19 +18,3 @@ gulp.task('lint', () => gulp.src(addDefSrcIgnore(['**/*.js', '*.json']), { dot: 
   .pipe($.eslint({ dotfiles: true }))
   .pipe($.eslint.format())
   .pipe($.eslint.failAfterError()));
-
-// Remove solutions from exercises
-gulp.task('remove-solutions', () => {
-  del.sync('dist');
-  return gulp.src(addDefSrcIgnore(['**', '!**/REMOVE{,/**}']), { dot: true })
-    .pipe($.replace(/^\s*(\/\/|<!--|\/\*)\s*REMOVE-START[\s\S]*?REMOVE-END\s*(\*\/|-->)?\s*$/gm, ''))
-    .pipe(gulp.dest('dist'));
-});
-
-// Prepare for distribution to students
-gulp.task('dist', ['lint', 'remove-solutions'], () => {
-  let npmConfig = require('./package.json');
-  delete npmConfig.scripts.install;
-  npmConfig = JSON.stringify(npmConfig, null, 2).replace(/-master/g, '');
-  fs.writeFileSync('dist/package.json', npmConfig);
-});
