db82ecbe3beff60104e2c5459942c979035361af
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-01 16:37:05 +0100

    feat(summary): add barebones skeleton to summary screen based on code example

--- client/screens/SummaryScreen.js
+++ client/screens/SummaryScreen.js
@@ -1,36 +1,74 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
-  ScrollView, StyleSheet, Text,
+  SectionList, StyleSheet, Text, View,
 } from 'react-native';
+import { uniq } from 'lodash';
+
+import { deleteExpense } from '../redux/actions';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
   },
-  contentContainer: {
-    paddingTop: 30,
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
-  contentText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
 
-export default class SummaryScreen extends React.Component {
+class SummaryScreen extends React.Component {
   static navigationOptions = {
     title: 'Summary',
   };
 
+  state = {
+    expenses: this.props.expenses,
+  }
+
+  computeSections = () => {
+    const dates = uniq(this.props.expenses.map(expense => expense.date));
+    console.log(dates);
+  }
+
   render() {
     return (
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <Text style={styles.contentText}>
-          WIP: Summary page
-        </Text>
-      </ScrollView>
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            { title: 'D', data: ['Devin'] },
+            { title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] },
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+          )}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  expenses: state.expenses,
+});
+
+const mapDispatchToProps = dispatch => ({
+  deleteExpense: expenseId => dispatch(deleteExpense(expenseId)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SummaryScreen);
