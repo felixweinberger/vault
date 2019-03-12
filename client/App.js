023a7871e2fc80e360573293ce162d65ebec10ae
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-12 13:37:43 +0100

    feat(env): add env support to app.js to handle clearing the async storage

--- client/App.js
+++ client/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { AsyncStorage } from 'react-native';
+import { ENV } from 'react-native-dotenv';
 
 import AppContainer from './containers/AppContainer';
 import configureStore from './redux/store';
 
-// Clear async storage for dev
-const clearAsyncStorage = async () => {
-  AsyncStorage.clear();
-};
-clearAsyncStorage();
+// Clear async storage for dev only
+if (ENV === 'dev') {
+  const clearAsyncStorage = async () => {
+    AsyncStorage.clear();
+  };
+  clearAsyncStorage();
+}
 
 const { store, persistor } = configureStore();
 
