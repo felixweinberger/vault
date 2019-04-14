123bd6c352bfa3fdd3ad4b09ba969e2ee9d6660d
Felix Weinberger <felix.weinberger@gmail.com>
2019-04-14 20:09:47 +0200

    build(ejection): attempting native build

--- client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -23,9 +23,13 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@felixweinberger/vault", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40felixweinberger%2Fvault%2F1.0.0%2F5344b5816b754c36cdd964e2e58508db-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
