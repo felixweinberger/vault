6998cb958f70ebccf0b6eb789a62d0825a28380d
Felix Weinberger <felix.weinberger@gmail.com>
2019-04-23 17:59:51 +0100

    build(build): add ios and android files generated from ejecting

--- client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -23,9 +23,12 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@felixweinberger/vault", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40felixweinberger%2Fvault%2F1.0.0%2F1f3befcd84000b08bfbcfec29a2eed3a-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
