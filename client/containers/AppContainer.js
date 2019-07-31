5dc8b86f212430c44afd98afb28c3618a14d81f9
Felix Weinberger <felix.weinberger@gmail.com>
2019-07-31 21:38:31 +0100

    feat: automatic backup to dropbox

--- client/containers/AppContainer.js
+++ client/containers/AppContainer.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import {
-  Platform, StatusBar, StyleSheet, View,
-} from 'react-native';
-import {
-  AppLoading, Font, Icon,
-} from 'expo';
+import React from "react";
+import { connect } from "react-redux";
+import { Platform, StatusBar, StyleSheet, View, AppState } from "react-native";
+import { AppLoading, Font, Icon } from "expo";
 
-import AppNavigator from '../navigation/AppNavigator';
-import { updateEntities } from '../redux/actions';
-import fetchFxRates from '../lib/fx';
+import AppNavigator from "../navigation/AppNavigator";
+import { updateEntities } from "../redux/actions";
+import fetchFxRates from "../lib/fx";
+import dropboxUpload from "../lib/sync";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-  },
+    backgroundColor: "white"
+  }
 });
 
 class App extends React.Component {
   state = {
-    isLoadingComplete: false,
+    isLoadingComplete: false
+  };
+
+  componentDidMount() {
+    AppState.addEventListener("change", this.handleAppStateChange);
+  }
+
+  handleAppStateChange = currentState => {
+    const {
+      state: {
+        entities: {
+          expenses,
+          settings: {
+            dropboxAuth: { accessToken },
+            automaticBackup
+          }
+        }
+      }
+    } = this.props;
+
+    if (automaticBackup && currentState === 'active') {
+      try {
+        dropboxUpload(accessToken, expenses);
+      } catch (_) {
+        // fail silently
+      }
+    };
+  };
+
+  updateFxRates = async () => {
+    const fxRates = await fetchFxRates();
+    const currencies = Object.keys(fxRates).reduce(
+      (accum, currency) => ({
+        ...accum,
+        [currency]: {
+          ...accum[currency],
+          fxRatePerEuro: fxRates[currency]
+        }
+      }),
+      this.props.state.entities.currencies
+    );
+    this.props.updateEntities({ currencies });
+  };
+
+  loadResourcesAsync = async () =>
+    Promise.all([
+      Font.loadAsync({ ...Icon.Ionicons.font }),
+      this.updateFxRates()
+    ]);
+
+  handleLoadingError = error => {
+    console.warn(error); // eslint-disable-line no-console
+  };
+
+  handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
   };
 
   render() {
@@ -35,45 +87,20 @@ class App extends React.Component {
     }
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
         <AppNavigator />
       </View>
     );
   }
-
-  updateFxRates = async () => {
-    const fxRates = await fetchFxRates();
-    const currencies = Object.keys(fxRates).reduce((accum, currency) => ({
-      ...accum,
-      [currency]: {
-        ...accum[currency],
-        fxRatePerEuro: fxRates[currency],
-      },
-    }), this.props.state.entities.currencies);
-    this.props.updateEntities({ currencies });
-  }
-
-  loadResourcesAsync = async () => Promise.all([
-    Font.loadAsync({ ...Icon.Ionicons.font }),
-    this.updateFxRates(),
-  ]);
-
-  handleLoadingError = (error) => {
-    console.warn(error); // eslint-disable-line no-console
-  };
-
-  handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
 
 const mapStateToProps = state => ({ state });
 
 const mapDispatchToProps = dispatch => ({
-  updateEntities: entities => dispatch(updateEntities(entities)),
+  updateEntities: entities => dispatch(updateEntities(entities))
 });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(App);
--- client/screens/SettingsScreen.js
+++ client/screens/SettingsScreen.js
@@ -1,88 +1,94 @@
 /* eslint-disable no-console */
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import {
-  ScrollView, StyleSheet, View, Text, TouchableOpacity, Alert,
-} from 'react-native';
-import { Linking, WebBrowser, Icon } from 'expo';
-import shittyQs from 'shitty-qs';
-import RNFetchBlob from 'rn-fetch-blob';
-import converter from 'json-2-csv';
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  Switch
+} from "react-native";
+import { Linking, WebBrowser, Icon } from "expo";
+import shittyQs from "shitty-qs";
+import RNFetchBlob from "rn-fetch-blob";
+import converter from "json-2-csv";
 
-import { updateEntities } from '../redux/actions';
-import { OAUTH_CONFIG, DROPBOX } from '../constants/Dropbox';
-import Colors from '../constants/Colors';
+import { updateEntities } from "../redux/actions";
+import { OAUTH_CONFIG, DROPBOX } from "../constants/Dropbox";
+import Colors from "../constants/Colors";
 
 const styles = StyleSheet.create({
   options: {
-    backgroundColor: Colors.greyLight,
+    backgroundColor: Colors.greyLight
   },
   option: {
     flex: 1,
     height: 60,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginLeft: 20,
-    marginRight: 20,
+    marginRight: 20
   },
   optionBtn: {
     padding: 5,
     width: 80,
-    alignItems: 'center',
+    alignItems: "center",
     borderWidth: 1,
     borderRadius: 10,
-    borderColor: 'grey',
+    borderColor: "grey"
   },
   sectionHeader: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     height: 40,
-    backgroundColor: Colors.orange6,
+    backgroundColor: Colors.orange6
   },
   sectionHeader__text: {
-    fontWeight: 'bold',
-    color: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    fontWeight: "bold",
+    color: "white",
+    justifyContent: "center",
+    alignItems: "center",
     fontSize: 16,
     marginLeft: 20,
-    marginRight: 20,
+    marginRight: 20
   },
   sectionHeader__icon: {
-    fontWeight: 'bold',
-    color: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    fontWeight: "bold",
+    color: "white",
+    justifyContent: "center",
+    alignItems: "center",
     fontSize: 25,
     marginLeft: 20,
-    marginRight: 20,
-  },
+    marginRight: 20
+  }
 });
 
 class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'Settings',
+    title: "Settings"
   };
 
   state = {
-    redirectData: null,
-  }
-
-  onCurrencyPress = () => {
-    this.props.navigation.navigate('SelectCurrency', { isGlobalChange: true });
+    redirectData: null
   };
 
-  handleRedirect = (event) => {
+  onCurrencyPress = () => {
+    this.props.navigation.navigate("SelectCurrency", { isGlobalChange: true });
+  };
+
+  handleRedirect = event => {
     WebBrowser.dismissBrowser();
-    console.log('Deep link event!', event);
+    console.log("Deep link event!", event);
 
     const queryStringResult = event.url.match(/#(.*)/);
     if (queryStringResult === null || queryStringResult.length < 2) {
       return Promise.reject(
-        new Error('Did not receive a query string as part of this deep link!'),
+        new Error("Did not receive a query string as part of this deep link!")
       );
     }
 
@@ -92,26 +98,28 @@ class SettingsScreen extends React.Component {
       const errorCode = parsedQueryString.error;
       const errorDescription = parsedQueryString.error_description;
 
-      console.log('Dropbox OAuth log! code:', errorCode);
-      console.log('Error description:', errorDescription);
+      console.log("Dropbox OAuth log! code:", errorCode);
+      console.log("Error description:", errorDescription);
 
       return Promise.reject(
-        new Error(`Could not authorize with Dropbox. Code: ${errorCode}`),
+        new Error(`Could not authorize with Dropbox. Code: ${errorCode}`)
       );
     }
 
     const accessToken = parsedQueryString.access_token;
     const accountId = parsedQueryString.account_id;
 
-    return this.props.updateEntities({ settings: { dropboxAuth: { accessToken, accountId } } });
+    return this.props.updateEntities({
+      settings: { dropboxAuth: { accessToken, accountId } }
+    });
   };
 
   addLinkingListener = () => {
-    Linking.addEventListener('url', this.handleRedirect);
+    Linking.addEventListener("url", this.handleRedirect);
   };
 
   removeLinkingListener = () => {
-    Linking.removeEventListener('url', this.handleRedirect);
+    Linking.removeEventListener("url", this.handleRedirect);
   };
 
   onDropboxLinkPress = async () => {
@@ -120,20 +128,24 @@ class SettingsScreen extends React.Component {
       const result = await WebBrowser.openBrowserAsync(
         [
           DROPBOX.AUTHORIZE_URL,
-          '?response_type=token',
+          "?response_type=token",
           `&client_id=${OAUTH_CONFIG.OAUTH_CLIENT_ID}`,
-          `&redirect_uri=${OAUTH_CONFIG.OAUTH_REDIRECT_URI_DEV}`,
-        ].join(''),
+          `&redirect_uri=${OAUTH_CONFIG.OAUTH_REDIRECT_URI_DEV}`
+        ].join("")
       );
       this.setState({ result });
       Alert.alert(
-        'Success',
-        'Your Dropbox account has been successfully linked! Your expenses can now be backed up to Dropbox/Apps/vault-expenses-app/backup.csv',
-        [{ text: 'OK' }],
+        "Success",
+        "Your Dropbox account has been successfully linked! Your expenses can now be backed up to Dropbox/Apps/vault-expenses-app/backup.csv",
+        [{ text: "OK" }]
       );
       this.removeLinkingListener();
     } catch (error) {
-      Alert.alert('Failure', 'An error occured communicating with Dropbox. Please try again later.', [{ text: 'OK' }]);
+      Alert.alert(
+        "Failure",
+        "An error occured communicating with Dropbox. Please try again later.",
+        [{ text: "OK" }]
+      );
       console.log(error);
     }
   };
@@ -142,29 +154,37 @@ class SettingsScreen extends React.Component {
     try {
       const { accessToken } = this.props.state.entities.settings.dropboxAuth;
       if (accessToken === null) {
-        throw new Error('Cannot unlink without an access token');
+        throw new Error("Cannot unlink without an access token");
       }
 
       const response = await fetch(DROPBOX.REVOKE_TOKEN_URL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
+          Authorization: `Bearer ${accessToken}`
+        }
       });
 
-      console.log('Unlink response:', response);
+      console.log("Unlink response:", response);
       if (response.status === 200) {
         this.props.updateEntities({
-          settings: { dropboxAuth: { accessToken: null, accountId: null } },
+          settings: { dropboxAuth: { accessToken: null, accountId: null } }
         });
-        Alert.alert('Success', 'Your Dropbox authorization has been sucessfully revoked.', [{ text: 'OK' }]);
+        Alert.alert(
+          "Success",
+          "Your Dropbox authorization has been sucessfully revoked.",
+          [{ text: "OK" }]
+        );
         return;
       }
-      Alert.alert('Failure', 'Something went wrong revoking your Dropbox authorization. please try again later.', [{ text: 'OK' }]);
+      Alert.alert(
+        "Failure",
+        "Something went wrong revoking your Dropbox authorization. please try again later.",
+        [{ text: "OK" }]
+      );
       throw new Error(
         `Failed to revoke Dropbox token. status: ${
           response.status
-        } and response: ${JSON.stringify(response)}`,
+        } and response: ${JSON.stringify(response)}`
       );
     } catch (error) {
       console.log(error);
@@ -176,57 +196,74 @@ class SettingsScreen extends React.Component {
       const { accessToken } = this.props.state.entities.settings.dropboxAuth;
       const { expenses } = this.props.state.entities;
       if (accessToken === null) {
-        throw new Error('Cannot perform backup without an access token');
+        throw new Error("Cannot perform backup without an access token");
       }
 
-      console.log('[Dropbox backup] UPLOADING and replacing DB on Dropbox: beginning.');
+      console.log(
+        "[Dropbox backup] UPLOADING and replacing DB on Dropbox: beginning."
+      );
 
       const jsonArr = Object.values(expenses);
       const csvStr = await converter.json2csvAsync(jsonArr);
       const backupExpenses = btoa(csvStr);
-      const response = await RNFetchBlob.fetch('POST', DROPBOX.UPLOAD_URL, {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/octet-stream',
-        'Dropbox-API-Arg': JSON.stringify({
-          path: '/backup.csv',
-          mode: 'overwrite',
-        }),
-      }, backupExpenses);
+      const response = await RNFetchBlob.fetch(
+        "POST",
+        DROPBOX.UPLOAD_URL,
+        {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/octet-stream",
+          "Dropbox-API-Arg": JSON.stringify({
+            path: "/backup.csv",
+            mode: "overwrite"
+          })
+        },
+        backupExpenses
+      );
 
-      Alert.alert('Success', 'Upload to Dropbox complete! Access your backup at Dropbox/Apps/vault-expenses-app/backup.csv', [{ text: 'OK' }]);
-      console.log('[Dropbox backup] UPLOAD to Dropbox complete!', response);
+      Alert.alert(
+        "Success",
+        "Upload to Dropbox complete! Access your backup at Dropbox/Apps/vault-expenses-app/backup.csv",
+        [{ text: "OK" }]
+      );
+      console.log("[Dropbox backup] UPLOAD to Dropbox complete!", response);
     } catch (e) {
-      Alert.alert('Failure', 'Failed to upload your backup to Dropbox. Please try again later.', [{ text: 'OK' }]);
-      console.log('Error: ', e);
+      Alert.alert(
+        "Failure",
+        "Failed to upload your backup to Dropbox. Please try again later.",
+        [{ text: "OK" }]
+      );
+      console.log("Error: ", e);
     }
-  }
+  };
 
   onDownloadPress = async () => {
     try {
       const { accessToken } = this.props.state.entities.settings.dropboxAuth;
       if (accessToken === null) {
-        throw new Error('Cannot download without an access token');
+        throw new Error("Cannot download without an access token");
       }
 
-      console.log('[Dropbox backup] DOWNLOADING and applying DB from Dropbox: beginning.');
+      console.log(
+        "[Dropbox backup] DOWNLOADING and applying DB from Dropbox: beginning."
+      );
 
-      const response = await RNFetchBlob.fetch('POST', DROPBOX.DOWNLOAD_URL, {
+      const response = await RNFetchBlob.fetch("POST", DROPBOX.DOWNLOAD_URL, {
         Authorization: `Bearer ${accessToken}`,
-        'Dropbox-API-Arg': JSON.stringify({
-          path: '/backup.csv',
-        }),
+        "Dropbox-API-Arg": JSON.stringify({
+          path: "/backup.csv"
+        })
       });
 
-      console.log('[Dropbox backup] DOWNLOAD from Dropbox complete!');
+      console.log("[Dropbox backup] DOWNLOAD from Dropbox complete!");
       const text = response.text();
 
       const jsonArr = await converter.csv2jsonAsync(text);
       const expenses = {};
-      jsonArr.forEach((expense) => {
+      jsonArr.forEach(expense => {
         expenses[expense.id] = expense;
       });
 
-      console.log('[Dropbox backup] CONVERSION to JSON complete!');
+      console.log("[Dropbox backup] CONVERSION to JSON complete!");
 
       const oldCategories = this.props.state.entities.categories;
       const categories = Object.values(expenses).reduce((acc, el) => {
@@ -234,68 +271,121 @@ class SettingsScreen extends React.Component {
         return acc;
       }, oldCategories);
       this.props.updateEntities({ expenses, categories });
-      Alert.alert('Success', 'Download from Dropbox complete! Your expenses now appear in the summary', [{ text: 'OK' }]);
-      console.log('[Dropbox backup] IMPORT from Dropbox complete!');
+      Alert.alert(
+        "Success",
+        "Download from Dropbox complete! Your expenses now appear in the summary",
+        [{ text: "OK" }]
+      );
+      console.log("[Dropbox backup] IMPORT from Dropbox complete!");
     } catch (e) {
-      Alert.alert('Failure', 'Failed to download your backup from Dropbox. Please try again later.', [{ text: 'OK' }]);
-      console.log('Error: ', e);
+      Alert.alert(
+        "Failure",
+        "Failed to download your backup from Dropbox. Please try again later.",
+        [{ text: "OK" }]
+      );
+      console.log("Error: ", e);
     }
-  }
+  };
+
+  onAutoBackupPress = () => {
+    this.props.updateEntities({
+      settings: {
+        automaticBackup: !this.props.state.entities.settings.automaticBackup
+      }
+    });
+  };
 
   render() {
     const { mainCurrency } = this.props.state.entities.settings;
-    const mainCurrencySymbol = this.props.state.entities.currencies[mainCurrency].symbol;
+    const mainCurrencySymbol = this.props.state.entities.currencies[
+      mainCurrency
+    ].symbol;
     return (
-      <ScrollView style={styles.options} contentContainerStyle={styles.optionsContainer}>
+      <ScrollView
+        style={styles.options}
+        contentContainerStyle={styles.optionsContainer}
+      >
         <View style={styles.sectionHeader}>
           <View style={styles.sectionHeader__left}>
-            <Text style={styles.sectionHeader__text}>
-              Currency settings
-            </Text>
+            <Text style={styles.sectionHeader__text}>Currency settings</Text>
           </View>
           <View style={styles.sectionHeader__right}>
-            <Icon.Ionicons name={'ios-cash'} style={styles.sectionHeader__icon}/>
+            <Icon.Ionicons
+              name={"ios-cash"}
+              style={styles.sectionHeader__icon}
+            />
           </View>
         </View>
         <View style={styles.option}>
           <Text>Home Currency</Text>
-          <TouchableOpacity style={styles.optionBtn} underlayColor='white' onPress={this.onCurrencyPress}>
-            <Text>{mainCurrency} ({mainCurrencySymbol})</Text>
+          <TouchableOpacity
+            style={styles.optionBtn}
+            underlayColor="white"
+            onPress={this.onCurrencyPress}
+          >
+            <Text>
+              {mainCurrency} ({mainCurrencySymbol})
+            </Text>
           </TouchableOpacity>
         </View>
         <View style={styles.sectionHeader}>
           <View style={styles.sectionHeader__left}>
-            <Text style={styles.sectionHeader__text}>
-              Dropbox integration
-            </Text>
+            <Text style={styles.sectionHeader__text}>Dropbox integration</Text>
           </View>
           <View style={styles.sectionHeader__right}>
-            <Icon.Ionicons name={'logo-dropbox'} style={styles.sectionHeader__icon}/>
+            <Icon.Ionicons
+              name={"logo-dropbox"}
+              style={styles.sectionHeader__icon}
+            />
           </View>
         </View>
         <View style={styles.option}>
           <Text style={styles.option__text}>Link Dropbox</Text>
-          <TouchableOpacity style={styles.optionBtn} underlayColor='white' onPress={this.onDropboxLinkPress}>
+          <TouchableOpacity
+            style={styles.optionBtn}
+            underlayColor="white"
+            onPress={this.onDropboxLinkPress}
+          >
             <Text style={styles.option__text}>Link</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.option}>
           <Text style={styles.option__text}>Unlink Dropbox</Text>
-          <TouchableOpacity style={styles.optionBtn} underlayColor='white' onPress={this.onDropboxUnlinkPress}>
+          <TouchableOpacity
+            style={styles.optionBtn}
+            underlayColor="white"
+            onPress={this.onDropboxUnlinkPress}
+          >
             <Text style={styles.option__text}>Unlink</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.option}>
           <Text style={styles.option__text}>Export CSV to Dropbox</Text>
-          <TouchableOpacity style={styles.optionBtn} underlayColor='white' onPress={this.onUploadPress}>
+          <TouchableOpacity
+            style={styles.optionBtn}
+            underlayColor="white"
+            onPress={this.onUploadPress}
+          >
             <Text style={styles.option__text}>Upload</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.option}>
           <Text style={styles.option__text}>Import CSV from Dropbox</Text>
-          <TouchableOpacity style={styles.optionBtn} underlayColor='white' onPress={this.onDownloadPress}>
+          <TouchableOpacity
+            style={styles.optionBtn}
+            underlayColor="white"
+            onPress={this.onDownloadPress}
+          >
             <Text style={styles.option__text}>Download</Text>
           </TouchableOpacity>
+        </View>
+        <View style={styles.option}>
+          <Text style={styles.option__text}>Automatic backup</Text>
+          <Switch
+            value={this.props.state.entities.settings.automaticBackup}
+            trackColor={{ true: Colors.orange6, false: "lightgrey" }}
+            onValueChange={this.onAutoBackupPress}
+          />
         </View>
       </ScrollView>
     );
@@ -305,10 +395,10 @@ class SettingsScreen extends React.Component {
 const mapStateToProps = state => ({ state });
 
 const mapDispatchToProps = dispatch => ({
-  updateEntities: entities => dispatch(updateEntities(entities)),
+  updateEntities: entities => dispatch(updateEntities(entities))
 });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(SettingsScreen);
