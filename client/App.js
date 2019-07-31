d15aba654ff62e026ec8e0a9cd1d90918d5ddc23
Felix Weinberger <felix.weinberger@gmail.com>
2019-07-31 22:15:46 +0100

    wip: setting up fastlane

--- client/App.js
+++ client/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { AsyncStorage } from "react-native";
-import { ENV } from "react-native-dotenv";
+// import { AsyncStorage } from "react-native";
+// import { ENV } from "react-native-dotenv";
 
 import AppContainer from "./containers/AppContainer";
 import configureStore from "./redux/store";
 
 // Clear async storage for test only
 console.disableYellowBox = true;
-if (ENV === "dev") {
-  const clearAsyncStorage = async () => {
-    AsyncStorage.clear();
-  };
-  clearAsyncStorage();
-}
+// if (ENV === "dev") {
+//   const clearAsyncStorage = async () => {
+//     AsyncStorage.clear();
+//   };
+//   clearAsyncStorage();
+// }
 
 const { store, persistor } = configureStore();
 
